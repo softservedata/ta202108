d9982ccd25d440bbcae5e7c33af2b0adf9c6b032
Softservedata <garasym@gmail.com>
2021-08-20 11:33:16 +0300

    Example. Java

--- src/com/softserve/edu/Example.java
+++ src/com/softserve/edu/Example.java
@@ -1,8 +1,24 @@
 package com.softserve.edu;
 
 public class Example {
+    private int k = 123;
+
+    public Example(int k1) {
+        k = k1;
+    }
     
+    public static void myPrint(Example ex) {
+        System.out.println("k= " + ex.k);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+        //
+        Example ex = new Example(222);
+        System.out.println("k = " + ex.k);
+        //myPrint(ex);
+        //
+        Example ex2 = new Example(3333);
+        myPrint(ex2);
     }
 }
