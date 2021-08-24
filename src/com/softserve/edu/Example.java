d885317c575c7ce1199d6b875d7777d56740070d
APietina <a.petina.chrome@gmail.com>
2021-08-25 01:28:54 +0300

    Second commit to apet0825

--- src/com/softserve/edu/Example.java
+++ src/com/softserve/edu/Example.java
@@ -3,6 +3,7 @@ package com.softserve.edu;
 public class Example {
     
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello my dear friends!");
+        System.out.println("Goodbye :)");
     }
 }
