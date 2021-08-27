2819bda27d69f8eb38ed0645e69d862dd963bc4c
tarnum777 <vyaryza@gmail.com>
2021-08-27 12:02:39 +0300

    Homework 3 (from 25.08.2021) code format

--- src/com/softserve/edu/hw3/Example.java
+++ src/com/softserve/edu/hw3/Example.java
@@ -6,21 +6,20 @@ import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int[] intArray = new int[3];
         for (int i = 0; i < 3; i++) {
             try {
                 System.out.println("Enter integer: ");
-                int myInt = sc.nextInt();
+                int myInt = scanner.nextInt();
                 intArray[i] = myInt;
             } catch (InputMismatchException e) {
                 System.out.println("Your input is invalid, please try again");
                 i--;
-                sc.next();
+                scanner.next();
             }
         }
         Arrays.sort(intArray);
-        System.out.println("Array sorted: ");
-        System.out.println(Arrays.toString(intArray));
+        System.out.println("Array sorted: " + Arrays.toString(intArray));
     }
 }
