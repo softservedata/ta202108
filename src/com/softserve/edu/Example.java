a730821968d0f1ab80b54cab0ab1958dda05b239
Viacheslav <viacheslav.kuzenko@gmail.com>
2021-08-27 09:31:53 +0300

    Added sort program

--- src/com/softserve/edu/Example.java
+++ src/com/softserve/edu/Example.java
@@ -1,10 +1,23 @@
 package com.softserve.edu;
 
-public class Example {
-    
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
+public class Example {
+
+    public static void main(String[] args) {
+        Scanner keybord = new Scanner(System.in);
+
+        int a = keybord.nextInt();
+        int b = keybord.nextInt();
+        int c = keybord.nextInt();
+
+        Integer[] arr = {a, b, c};
+        Arrays.sort(arr, Collections.reverseOrder());
+        System.out.println(Arrays.toString(arr));
+    }
 }
-// code compiled successfully
+
+
+
