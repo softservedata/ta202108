f0d515ad41c8682c5eca395701fc8026f6d8c4ed
tarnum777 <vyaryza@gmail.com>
2021-08-27 12:00:05 +0300

    Homework 3 (from 25.08.2021) 1) Прочитати з клавіатури три числа (цілих або дійсних).    Вивести ці числа на екран у порядку зростання.    Для коду використати пакет com.softserve.edu.hw3    Запушати код на github у свою вітку

--- src/com/softserve/edu/hw3/Example.java
+++ src/com/softserve/edu/hw3/Example.java
@@ -5,9 +5,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Example {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int[] intArray = new int[3];
         for (int i = 0; i < 3; i++) {
@@ -24,6 +22,5 @@ public class Example {
         Arrays.sort(intArray);
         System.out.println("Array sorted: ");
         System.out.println(Arrays.toString(intArray));
-
     }
 }
