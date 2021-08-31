fc84d203540799ba64867a3f08a3563cb668f7b9
YuriiTA <79091613+yura-test-qa@users.noreply.github.com>
2021-08-31 20:16:23 +0300

    upd test example.java

--- src/com/softserve/edu/Example.java
+++ src/com/softserve/edu/Example.java
@@ -3,6 +3,6 @@ package com.softserve.edu;
 public class Example {
     
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Test message");
     }
 }
