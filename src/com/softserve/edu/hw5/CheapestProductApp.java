ea5b14a4494026f55d4fcf5f1f03a51d76792e81
tarnum777 <vyaryza@gmail.com>
2021-09-04 13:12:42 +0300

    Homework 5 (from 01.09.2021) 1) Використати клас Product з Homework 4.    В методі main створити масив із десяти екземплярів типу Product (можна захардкодити).    Вивести назву продукту (або продуктів) з найменшою ціною.    Для коду використати пакет com.softserve.edu.hw5    Запушати код на github у свою вітку.

---
+++ src/com/softserve/edu/hw5/CheapestProductApp.java
@@ -0,0 +1,29 @@
+package com.softserve.edu.hw5;
+
+import com.softserve.edu.hw4.Product;
+
+public class CheapestProductApp {
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("product1", 5.6f);
+        products[1] = new Product("product2", 55.6f);
+        products[2] = new Product("product3", 1);
+        products[3] = new Product("product4", 55.6f);
+        products[4] = new Product("product5", 8);
+        products[0] = new Product("product6", 5444.6f);
+        products[1] = new Product("product7", 55.6f);
+        products[2] = new Product("product8", 0.88f);
+        products[3] = new Product("product9", 55.6f);
+        products[4] = new Product("product10", 8);
+
+        Product cheapestProduct = products[0];
+        float leastPrice = cheapestProduct.getPrice();
+        for(Product product: products) {
+            if (product.getPrice() < leastPrice) {
+                leastPrice = product.getPrice();
+                cheapestProduct = product;
+            }
+        }
+        System.out.println("Product with least price is: " + cheapestProduct.getName());
+    }
+}
