05cbd362abbb027cc93389a7e345987c51b8d80d
APietina <a.petina.chrome@gmail.com>
2021-09-10 21:54:44 +0300

    HomeWork #5:  - create array of products  - find and print product with the lowest price

--- src/com/softserve/edu/hw5/AppProductMinPrice.java
+++ src/com/softserve/edu/hw5/AppProductMinPrice.java
@@ -10,12 +10,12 @@ public class AppProductMinPrice {
 
     public static void main (String[] args) {
         Product[] Products;
-        Product min = new Product();
+        Product[] CheapProducts;
 
         Products = configProductsArray();     //how many Products you'll need
         readProducts(Products);               //enter products
-        min = findMinProductPrice(Products);  //find the cheapest product
-        printMinPriceProduct(min);            //print the cheapest product
+        CheapProducts = findMinProductPrice(Products);  //find the cheapest product
+        printMinPriceProduct(CheapProducts);            //print the cheapest product
     }
 
     //how many Products you'll need
@@ -39,6 +39,7 @@ public class AppProductMinPrice {
     private static void readProducts(Product[] Products) {
         for (int i=0; i<Products.length; i++) {
             Products[i] = new Product();
+
             System.out.println("Enter Product" + (i+1));
             Scanner in = new Scanner(System.in);
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -63,23 +64,57 @@ public class AppProductMinPrice {
     }
 
     //find the cheapest product
-    private static Product findMinProductPrice(Product[] Products) {
-        Product min = new Product();
-        int imin = 0;
-        min = Products[imin];
+    private static Product[] findMinProductPrice(Product[] Products) {
 
-        for (int i=0; i<Products.length; i++) {
-            if(Products[i].getPrice() < Products[imin].getPrice()) {
+        Product minPriceProduct;
+        Product[] TempProducts = new Product[Products.length];  //temporary array of the cheapest products
+        int imin = 0;
+        int j = 0;
+        minPriceProduct = Products[imin];
+
+        for (int i=1; i<Products.length; i++) {           //find the lowest price value
+            if(minPriceProduct.getPrice() > Products[i].getPrice()) {
                 imin = i;
-                min = Products[i];
+                minPriceProduct = Products[imin];
             }
         }
-        return min;
+
+        TempProducts[0] = minPriceProduct;
+        for (int i=1; i<Products.length; i++) {             //find all cheapest products
+            if (TempProducts[0].getPrice() == Products[i].getPrice()) {
+                if(i != imin){
+                    j++;
+                    TempProducts[j] = Products[i];
+                }
+            }
+        }
+
+        Product[] CheapProducts = new Product[j+1];         //final array with the cheapest products ready to be printed
+        CheapProducts[0] = TempProducts[0];
+        for (int i = 0; i<j+1; i++) {
+            CheapProducts[i] = TempProducts[i];
+        }
+
+        return CheapProducts;
     }
 
     //print the cheapest product
-    private static void printMinPriceProduct(Product min) {
-        System.out.println("The cheapest product is " + min.getName() + " with price = " + min.getPrice());
+    private static void printMinPriceProduct(Product[] CheapProducts) {
+        if(CheapProducts.length == 1) {
+            System.out.println("The cheapest product is " + CheapProducts[0].getName() + " with price = " + CheapProducts[0].getPrice());
+        }
+        else {
+            if(CheapProducts.length > 1){
+                System.out.println("The cheapest products are:");
+                for(int i = 0; i<CheapProducts.length; i++){
+                    System.out.println("---" + CheapProducts[i].getName() + " with price = " + CheapProducts[i].getPrice());
+                }
+            }
+            else {
+                if(CheapProducts.length < 1) {
+                    System.out.println("Error");
+                }
+            }
+        }
     }
-
 }
