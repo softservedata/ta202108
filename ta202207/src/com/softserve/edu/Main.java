aa1fa725403cddf8d02d77e2a463dbf003172931
NataliTaras <n.puchka@gmail.com>
2022-07-31 22:50:28 +0300

    Third try

--- ta202207/src/com/softserve/edu/Main.java
+++ ta202207/src/com/softserve/edu/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
                 System.out.println("Hello, how are you?");
                 System.out.println("Im bad");
+                System.out.println("And angry");
+
     }
 }
