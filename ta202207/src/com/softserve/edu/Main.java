510c5d551a1c840bc7c87c20fa33d8b32eb1dec8
NataliTaras <ntaras@gmail.com>
2022-07-31 23:36:01 +0300

    4th try

--- ta202207/src/com/softserve/edu/Main.java
+++ ta202207/src/com/softserve/edu/Main.java
@@ -4,8 +4,6 @@ package com.softserve.edu;
 public class Main {
     public static void main(String[] args) {
                 System.out.println("Hello, how are you?");
-                System.out.println("Im bad");
-                System.out.println("And angry");
 
     }
 }
